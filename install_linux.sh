16f7136f345ae402573b5ad15bbe06c6fd9db144
Loic Royer <loic.royer@czbiohub.org>
2020-10-29 15:43:53 -0700

    wip

--- install_linux.sh
+++ install_linux.sh
@@ -1,21 +1,26 @@
-conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch
-conda install -y dask zarr 
-conda update -y dask zarr pytorch
+conda deactivate
+conda create -y --name dexp python=3.8
+conda activate dexp
 
-conda config --add channels conda-forge
-conda install -c conda-forge ocl-icd-system
-conda install pyopencl
-conda install -y tifffile
-
-pip install cupy-cuda102 --upgrade
-pip install torch-dct --upgrade
-pip install scikit-image --upgrade
-pip install tensorflow-gpu==1.15 keras==2.2.4 --upgrade
-pip install napari[all] --upgrade
-pip install click cachey numexpr joblib --upgrade
-pip install gputools dtcwt csbdeep --upgrade
-pip install git+https://github.com/guiwitz/naparimovie.git@master#egg=naparimovie --upgrade
-pip install spimagine --upgrade
-pip install pytest --upgrade
-pip install -e .
+#conda config --add channels conda-forge
+#
+#conda install -y -c pytorch pytorch torchvision cudatoolkit=11.0.221
+#conda install -y -c conda-forge cupy
+#conda install -y -c conda-forge dask zarr
+#conda install -y -c conda-forge ocl-icd-system
+#conda install -y -c conda-forge pyopencl
+#conda install -y -c conda-forge tifffile
+#conda update -y pytorch torchvision cudatoolkit cupy dask zarr ocl-icd-system pyopencl tifffile
+#
+##pip install cupy-cuda102 --upgrade
+#pip install torch-dct --upgrade
+#pip install scikit-image --upgrade
+##pip install tensorflow-gpu==1.15 keras==2.2.4 --upgrade
+#pip install napari[all] --upgrade
+#pip install click cachey numexpr joblib --upgrade
+##pip install gputools dtcwt csbdeep --upgrade
+#pip install git+https://github.com/guiwitz/naparimovie.git@master#egg=naparimovie --upgrade
+#pip install spimagine --upgrade
+#pip install pytest --upgrade
+#pip install -e .
 
