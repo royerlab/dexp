a41def068b06152684965bcd33f16c19c4e63a78
Loic Royer <loic.royer@czbiohub.org>
2020-12-08 15:47:57 -0800

    small fix

--- dexp/cli/commands/speedtest.py
+++ dexp/cli/commands/speedtest.py
@@ -11,16 +11,18 @@ def speedtest():
         import subprocess
         cwd = os.getcwd()
 
+        filename = 'zzspeedtestfilezz'
+
         with asection("Write speed:"):
-            result = subprocess.run(['time', 'sh', '-c', f"dd if=/dev/zero of={cwd}/__speedtestfile__ bs=4M count=256 && sync" ], capture_output=True)
+            result = subprocess.run(['time', 'sh', '-c', f'dd if=/dev/zero of="{cwd}/{filename}" bs=4M count=256 && sync' ], capture_output=True)
             #aprint(result.stdout.decode("utf-8").split('\n'))
             aprint(result.stderr.decode("utf-8").split('\n')[2])
 
         with asection("Read speed:"):
-            result = subprocess.run(['time', 'sh', '-c', f"dd if={cwd}/__speedtestfile__ of=/dev/null bs=4M count=256 && sync"], capture_output=True)
+            result = subprocess.run(['time', 'sh', '-c', f'dd if="{cwd}/{filename}" of=/dev/null bs=4M count=256 && sync'], capture_output=True)
             #aprint(result.stdout.decode("utf-8").split('\n'))
             aprint(result.stderr.decode("utf-8").split('\n')[2])
 
-        os.remove(f"{cwd}/__speedtestfile__")
+        os.remove(f"{cwd}/{filename}")
 
 
