9a6fbd826a8356012bc2352ec2fed2af57a67694
Jordão Bragantini <jordao.bragantini@czbiohub.org>
2022-03-18 11:03:09 -0700

    updated api docs generation (#93)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,8 +48,9 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ["_templates", "_build"]
 
+autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # -- Options for HTML output -------------------------------------------------
 
